dddd662786fd74dbee9c55fe8d3920844b637e0b
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-04-25 14:14:14 -0400

    add write file function

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const createReadme = require('./prompt-questions.js');
-//const writeFile = require('./assets/JS/generateReadMe.js')
+const createReadme = require('./Assets/JS/prompt-questions.js');
+//then.writeFile = require('./assets/JS/generateReadMe.js')
