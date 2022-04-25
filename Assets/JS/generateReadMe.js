c669ea1bacff5cb467ea52707c6dbc4606e985b0
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-04-25 12:49:01 -0400

    added readme sections

--- Assets/JS/generateReadMe.js
+++ Assets/JS/generateReadMe.js
@@ -1,6 +1,21 @@
 function generateReadme(answers) {
     return `
 <h1>${answers.title}</h1>
+
+## Description
+## Table of Contents
+## Installation
+## Usage
+## Credits
+## License
+## Badges
+## Features
+## How to Contribute
+## Tests
+## Questions?
+    Contact me: ${answers.email}
+    Github: ${answers.username}
+
     `
 }
 
